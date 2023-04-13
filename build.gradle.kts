18bc6d3cf2c0dc615c57d6f76ad5dc1087668368
CXwudi <cx2010017@gmail.com>
2023-04-13 19:37:01 -0400

    init

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    kotlin("jvm") version "1.6.21"
+    kotlin("kapt") version "1.6.21"
+    kotlin("plugin.allopen") version "1.6.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("io.micronaut.application") version "3.7.8"
+}
+
+version = "0.1"
+group = "com.example"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    micronautBoms(platform("io.micronaut:micronaut-bom:3.8.9"))
+    kapt("io.micronaut:micronaut-http-validation")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-jackson-databind")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
+    runtimeOnly("ch.qos.logback:logback-classic")
+    implementation("io.micronaut:micronaut-validation")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+}
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+kotlin {
+    target {
+        compilations.all {
+            kotlinOptions.jvmTarget = "17"
+        }
+    }
+}
+
+graalvmNative.toolchainDetection.set(false)
+micronaut {
+    runtime("netty")
+    testRuntime("kotest")
+    processing {
+        incremental(true)
+        annotations("com.example.*")
+    }
+}
